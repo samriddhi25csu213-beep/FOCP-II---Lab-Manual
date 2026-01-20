13975eaf146a3b0997270531c817bc838c211641
samriddhi25csu213-beep <samriddhi25csu213@ncuindia.edu>
2026-01-20 10:56:01 +0530

    Add program to calculate area of a circle

---
+++ DAY2/Q2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float r, area;
+    cout << "enter the radius: ";
+    cin >> r;
+    area = 3.14*r*r;
+    cout << "\narea of the circular fountain is: " << area;
+    return 0;
+}
