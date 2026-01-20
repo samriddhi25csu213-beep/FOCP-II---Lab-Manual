d9b145c3cf25d01ebdbb93d3b2c10941eab758e2
samriddhi25csu213-beep <samriddhi25csu213@ncuindia.edu>
2026-01-20 10:58:07 +0530

    number swapping logic
    Implement a program to swap two numbers using multiplication and division.

---
+++ DAY2/Q5.cpp
@@ -0,0 +1,20 @@
+include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int a, b, c;
+   
+    cout << "enter the value of a: ";
+    cin >> a;
+    cout << "enter the value of b: ";
+    cin >> b;
+    // c = a;
+    // a = b;
+    // b = c;
+    //swap(a, b);
+    // a = a + b;
+    // b = a - b;
+    // a = a - b;
+    a = a * b;
+    b = a / b;
+    a = a / b;
