5a2cd9abd8d242d2fbaff0823e59f9b8b4adc978
samriddhi25csu213-beep <samriddhi25csu213@ncuindia.edu>
2026-01-20 10:53:57 +0530

    Add temperature conversion from Fahrenheit to Celsius

---
+++ DAY1/Q3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+ float f, c;
+cout << "Writ the tempertaure in degree fahrenhiet ";
+cin >> f;
+c = (f - 32)*5/9;
+cout << "\ntemperature in degree celcius is " << c;
+return 0;
+}
